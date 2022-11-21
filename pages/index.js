5b713ecf962dad96d383e80af5b04e14b9aaea06
Sara Ramírez Andrade <sararamirezandrade@gmail.com>
2022-11-20 23:55:42 -0300

    dark mode

--- pages/index.js
+++ pages/index.js
@@ -12,11 +12,11 @@ import web3 from '../public/web3.png'
 import web4 from '../public/web4.png'
 import web5 from '../public/web5.png'
 import web6 from '../public/web6.png'
+import { BsFillSunFill } from 'react-icons/bs'
 import { useState } from 'react';
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
-
 
 
 
@@ -30,15 +30,20 @@ export default function Home() {
       <main className='bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900'>
         <section className=" min-h-screen" >
           <nav className="py-10 mb-12 flex justify-between">
-            <h1 className='text-xl font-burtons'>
+            <h1 className='text-xl font-burtons dark:text-white'>
               developed
             </h1>
             <ul className='flex items-center'>
               <li>
-                <BsFillMoonStarsFill
-                  onClick={() => setDarkMode(!darkMode)}
-                  className='cursor-pointer text-2xl'
-                />
+                {!darkMode
+                  ? <BsFillMoonStarsFill
+                    onClick={() => setDarkMode(!darkMode)}
+                    className='cursor-pointer text-2xl'
+                  />
+                  : <BsFillSunFill
+                    onClick={() => setDarkMode(!darkMode)}
+                    className='cursor-pointer text-2xl dark:text-white' />
+                }
               </li>
               <li>
                 <a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="#">Resume</a>
@@ -49,10 +54,10 @@ export default function Home() {
             <h2 className='text-5xl py-2 text-teal-600 font-medium md:text-6xl'>
               Dimitri Marco
             </h2>
-            <h3 className='text-2xl py-2 md:text-3xl'>
+            <h3 className='text-2xl py-2 md:text-3xl  dark:text-white'>
               Developer and designer
             </h3>
-            <p className='text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto'>
+            <p className='text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto dark:text-white'>
               Freelancer providing services for programming and design content needs. Join me down belor and let's get cracking!</p>
           </div>
           <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600'>
@@ -67,12 +72,14 @@ export default function Home() {
 
         <section>
           <div>
-            <h3 className='text-3xl py-1'>Services I Offer</h3>
-            <p className='text-md py-2 leading-8 text-gray-800'>
+            <h3 className='text-3xl py-1 dark:text-white'>
+              Services I Offer
+            </h3>
+            <p className='text-md py-2 leading-8 text-gray-800 dark:text-white'>
               Since the beginning of my jouney as a freelance designer and developer, I've done remote work for <span className='text-teal-500'>agencies </span>
               consulted for <span className='text-teal-500'>startups </span> and collaborated woth talented people to create digital products for both business and consumer use.
             </p>
-            <p className='text-md py-2 leading-8 text-gray-800'>
+            <p className='text-md py-2 leading-8 text-gray-800 dark:text-white'>
               I offer from a wide range of services, including brand design, programing and teaching.
             </p>
           </div>
@@ -88,7 +95,7 @@ export default function Home() {
               <p className='text-gray-800 py-1'>Photoshop</p>
               <p className='text-gray-800 py-1'>Figma</p>
             </div>
-            <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+            <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white'>
               <Image src={code} width={100} height={100} style={{ display: 'inline-flex' }} />
               <h3 className='text-lg font-medium pt-8 pb-2'>Beautiful designs</h3>
               <p className='py-2'>
@@ -99,7 +106,7 @@ export default function Home() {
               <p className='text-gray-800 py-1'>Photoshop</p>
               <p className='text-gray-800 py-1'>Figma</p>
             </div>
-            <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+            <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white'>
               <Image src={consulting} width={100} height={100} style={{ display: 'inline-flex' }} />
               <h3 className='text-lg font-medium pt-8 pb-2'>Beautiful designs</h3>
               <p className='py-2'>
@@ -114,12 +121,14 @@ export default function Home() {
         </section>
         <section>
           <div>
-            <h3 className='text-3xl py-1'>Porfolio</h3>
-            <p className='text-md py-2 leading-8 text-gray-800'>
+            <h3 className='text-3xl py-1 dark:text-white'>
+              Porfolio
+            </h3>
+            <p className='text-md py-2 leading-8 text-gray-800 dark:text-white'>
               Since the beginning of my jouney as a freelance designer and developer, I've done remote work for <span className='text-teal-500'>agencies </span>
               consulted for <span className='text-teal-500'>startups </span> and collaborated woth talented people to create digital products for both business and consumer use.
             </p>
-            <p className='text-md py-2 leading-8 text-gray-800'>
+            <p className='text-md py-2 leading-8 text-gray-800 dark:text-white'>
               I offer from a wide range of services, including brand design, programing and teaching.
             </p>
           </div>
@@ -138,8 +147,7 @@ export default function Home() {
                 className="rounded-lg object-cover"
                 width={'100%'}
                 height={'100%'}
-                layout="responsive"
-              />
+                layout="responsive" />
             </div>
             <div className='basis-1/3 flex-1'>
               <Image
@@ -147,8 +155,7 @@ export default function Home() {
                 className="rounded-lg object-cover"
                 width={'100%'}
                 height={'100%'}
-                layout="responsive"
-              />
+                layout="responsive" />
             </div>
             <div className='basis-1/3 flex-1'>
               <Image
@@ -156,8 +163,7 @@ export default function Home() {
                 className="rounded-lg object-cover"
                 width={'100%'}
                 height={'100%'}
-                layout="responsive"
-              />
+                layout="responsive" />
             </div>
             <div className='basis-1/3 flex-1'>
               <Image
